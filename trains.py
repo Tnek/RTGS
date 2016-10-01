b413b316746f958e65f21298ccbd5402d002f20e
Kent Ma <kentma@nyu.edu>
2016-10-01 19:36:37 -0400

    Finished implementing dijkstra's algorithm

--- trains.py
+++ trains.py
@@ -5,15 +5,28 @@ class Sector(object):
         self.name = name
         self.left = []
         self.right = []
-        self.canReach = True
+        self.reset_astar()
+
+    def reset_astar(self):
+        self.weight = 0
+
+        self._parent = None
+        # Temp value used by astar
+        self._distance = 0
+
+
+    def can_reach(self, other):
+        return True
     def __repr__(self):
-        return self.name
+        if self._parent != None:
+            return self.name + "->" + str(self._parent)
+        else:
+            return self.name
 
 class Blocker(Sector):
     def __init__(self, name):
         super().__init__(name)
-    def __repr__(self):
-        return self.name
+        self.enabled = False
 
 connections = {
     "L1":("S1",),
@@ -34,19 +47,63 @@ connections = {
     "bI":("S7",), "bJ":("S8",), "bL":("S8",), "bK":("R1",)
 }
 
-gen = lambda leadchr, rnge:{leadchr+str(i):Sector(leadchr+str(i)) for i in rnge}
-sectors = gen("S", range(1, 9))
-rstations = gen("R", range(1, 4))
-lstations = gen("L", range(1 ,4))
-blockers = {"b"+chr(i):Blocker("b"+chr(i)) for i in range(65, 77)}
-objects = {**sectors, **rstations, **lstations, **blockers}
+def reverse_running_mode():
+    gen = lambda leadchr, rnge:{leadchr+str(i):Sector(leadchr+str(i)) for i in rnge}
+    sectors = gen("S", range(1, 9))
+    rstations = gen("R", range(1, 4))
+    lstations = gen("L", range(1 ,4))
+    blockers = {"b"+chr(i):Blocker("b"+chr(i)) for i in range(65, 77)}
+    objects = {**sectors, **rstations, **lstations, **blockers}
 
-#: Populate graph given table of connections
-for connect in connections:
-    workingobj = objects[connect]
-    children = connections[connect]
-    for i in children:
-        workingobj.right.append(objects[i])
-        objects[i].left.append(workingobj)
+    #: Populate graph given table of connections
+    for connect in connections:
+        workingobj = objects[connect]
+        children = connections[connect]
+        for i in children:
+            workingobj.right.append(objects[i])
+            objects[i].left.append(workingobj)
+#    for i in rstations:
+#        print(astar(rstations[i], lstations["L1"]))
+    for i in lstations:
+        print(astar(lstations[i], rstations["R1"]))
+    for i in objects:
+        objects[i].reset_astar()
+    for i in rstations:
+        print(astar(rstations[i], lstations["L1"]))
 
+def astar(start, end):
+    """ Actually a lie, this is actually just dijkstra's because I don't have a heuristic to use yet """
+    openset = {start}
+    while len(openset) > 0:
+        for i in openset:
+            min_weight = None
+            if min_weight == None or i.weight < min_weight:
+                current_node = i
+                min_weight = i.weight
 
+        # We made it!
+        if current_node.name[0] == end.name[0]:
+            return current_node
+
+        openset.remove(current_node)
+
+        possible_moves = []
+        # Which direction?
+        if start.name[0] == "L":  # We want to go right
+            possible_moves = [i for i in current_node.right if current_node.can_reach(i)] 
+        elif start.name[0] == "R":  # We want to go left
+            possible_moves = [i for i in current_node.left if current_node.can_reach(i)]
+
+        for move in possible_moves:
+            move._distance = current_node._distance + 1
+
+            if not move in openset:
+                openset.add(move)
+            else:
+                continue
+
+            move._parent = current_node
+            #: TODO: Actually write a heuristic
+            move.weight = move._distance # + any other heuristics
+
+reverse_running_mode()
